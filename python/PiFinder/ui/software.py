8869aaa0d7ae8698dafba80191dc67924c71076f
testezt <eztastro@gmail.com>
2026-03-16 21:51:50 +0900

    url

--- python/PiFinder/ui/software.py
+++ python/PiFinder/ui/software.py
@@ -72,7 +72,7 @@ class UISoftware(UIModule):
         """
         try:
             res = requests.get(
-                "https://raw.githubusercontent.com/brickbots/PiFinder/release/version.txt"
+                "https://raw.githubusercontent.com/testezt/EZTFinder5/refs/heads/main/version.txt"
             )
         except requests.exceptions.ConnectionError:
             print("Could not connect to github")
