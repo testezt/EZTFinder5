9a2c764bc4d3dee9be96a6541a2358481a85c508
testezt <eztastro@gmail.com>
2026-03-18 14:40:20 +0900

    Update pifinder_post_update.sh

--- pifinder_post_update.sh
+++ pifinder_post_update.sh
@@ -2,42 +2,42 @@
 #sudo pip install -r /home/pifinder/EZTFinder5/python/requirements.txt
 
 # Set up migrations folder if it does not exist
-if ! [ -d "/home/pifinder/PiFinder_data/migrations" ]
-then
-    mkdir /home/pifinder/PiFinder_data/migrations
-fi
+#if ! [ -d "/home/pifinder/PiFinder_data/migrations" ]
+#then
+#    mkdir /home/pifinder/PiFinder_data/migrations
+#fi
 
 # v1.x.x
 # everying prior to selecitve migrations
-if ! [ -f "/home/pifinder/PiFinder_data/migrations/v1.x.x" ]
-then
-    source /home/pifinder/EZTFinder5/migration_source/v1.x.x.sh
-    touch /home/pifinder/PiFinder_data/migrations/v1.x.x
-fi
+#if ! [ -f "/home/pifinder/PiFinder_data/migrations/v1.x.x" ]
+#then
+#    source /home/pifinder/EZTFinder5/migration_source/v1.x.x.sh
+#    touch /home/pifinder/PiFinder_data/migrations/v1.x.x
+#fi
 
 # v2.1.0
 # Switch to Cedar
-if ! [ -f "/home/pifinder/PiFinder_data/migrations/v2.1.0" ]
-then
-    source /home/pifinder/EZTFinder5/migration_source/v2.1.0.sh
-    touch /home/pifinder/PiFinder_data/migrations/v2.1.0
-fi
+#if ! [ -f "/home/pifinder/PiFinder_data/migrations/v2.1.0" ]
+#then
+#    source /home/pifinder/EZTFinder5/migration_source/v2.1.0.sh
+#    touch /home/pifinder/PiFinder_data/migrations/v2.1.0
+#fi
 
 # v2.2.1
 # Install libinput
-if ! [ -f "/home/pifinder/PiFinder_data/migrations/v2.2.1" ]
-then
-    source /home/pifinder/EZTFinder5/migration_source/v2.2.1.sh
-    touch /home/pifinder/PiFinder_data/migrations/v2.2.1
-fi
+#if ! [ -f "/home/pifinder/PiFinder_data/migrations/v2.2.1" ]
+#then
+#    source /home/pifinder/EZTFinder5/migration_source/v2.2.1.sh
+#    touch /home/pifinder/PiFinder_data/migrations/v2.2.1
+#fi
 
 # v2.2.2
 # Enable host usb on usb-c port
-if ! [ -f "/home/pifinder/PiFinder_data/migrations/v2.2.2" ]
-then
-    source /home/pifinder/EZTFinder5/migration_source/v2.2.2.sh
-    touch /home/pifinder/PiFinder_data/migrations/v2.2.2
-fi
+#if ! [ -f "/home/pifinder/PiFinder_data/migrations/v2.2.2" ]
+#then
+#    source /home/pifinder/EZTFinder5/migration_source/v2.2.2.sh
+#    touch /home/pifinder/PiFinder_data/migrations/v2.2.2
+#fi
 
 # DONE
 echo "Post Update Complete"
